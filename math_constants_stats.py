184b33f926bcf45873e430082e3350b417c002e7
Asaf Ravid <46109899+asafravid@users.noreply.github.com>
2025-05-11 17:59:34 +0300

    Update math_constants_stats.py

--- math_constants_stats.py
+++ math_constants_stats.py
@@ -3,8 +3,8 @@ from collections import Counter
 import os
 
 # Set precision to digits
-mp.dps = 100000
-NUM_ELEMENTS_TO_PLOT = 25
+mp.dps = 1000000
+NUM_ELEMENTS_TO_PLOT = 20
 
 def plot_frequencies_text_color(freq_data, title, xlabel, color, num_elements):
     max_width = 345  # Maximum width of the bar in characters
